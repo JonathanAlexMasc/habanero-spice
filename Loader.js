96471dd788a98b159cc74d8ba6b8e8259447dd27
Mascarenhas <Jonathan.Mascarenhas@mines.sdsmt.edu>
2024-11-27 15:45:06 -0700

    removed linux builds

--- Loader.js
+++ Loader.js
@@ -2,7 +2,7 @@ const { app, BrowserWindow, ipcMain, dialog } = require('electron');
 const path = require('path');
 const fs = require('fs');
 const { exec } = require('child_process');
-const autoUpdater = require("electron-updater");
+const { autoUpdater } = require("electron-updater");
 
 let win; 
 
